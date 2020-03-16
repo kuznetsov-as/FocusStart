f7ba1ba8e5648d3c7ed53acb1bf8a96e28b2ba3c
deusestmortuus <kuznetsov1398@gmail.com>
2020-03-16 15:53:34 +0700

    Исправил замечания

--- src/main/java/cft/focus/Controller.java
+++ src/main/java/cft/focus/Controller.java
@@ -8,20 +8,23 @@ public class Controller {
 
     public static void main(String[] args) {
 
-        int tableSize;
         System.out.println("Пожалуйста, введите размер таблицы:");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         while (true) {
             try {
-                tableSize = Integer.parseInt(reader.readLine());
+                int tableSize = Integer.parseInt(reader.readLine());
                 if (tableSize > 32) {
                     System.out.println("Введенное число не должно быть больше 32");
                 } else if (tableSize < 1) {
                     System.out.println("Введенное число не должно быть меньше 1");
                 } else {
-                    TableFormat.printMultiplicationTable(MultiplicationTable.calculateMultiplication(tableSize), tableSize);
-                    reader.close();
+                    TableFormatter tableFormatter = new TableFormatter();
+                    MultiplicationTable multiplicationTable = new MultiplicationTable();
+                    TableDisplay tableDisplay = new TableDisplay();
+
+                    String result = tableFormatter.formatForMultiplicationTable(multiplicationTable.calculateMultiplication(tableSize), tableSize);
+                    tableDisplay.printTable(result);
                     break;
                 }
             } catch (NumberFormatException | IOException e) {
--- src/main/java/cft/focus/MultiplicationTable.java
+++ src/main/java/cft/focus/MultiplicationTable.java
@@ -2,33 +2,31 @@ package cft.focus;
 
 public class MultiplicationTable {
 
-    /*
-    *   Метод рассчитывает и возвращает двумерный массив со всеми табличными значениями.
-    *   Тип данных String выбран в связи с удобством дальнейшего форматирования.
-    * */
+    /**
+     * Метод рассчитывает и возвращает двумерный массив со всеми табличными значениями.
+     *
+     * @param tableSize размер таблицы
+     * @return двумерный массив с таблицей умножения
+     */
 
-    public static String[][] calculateMultiplication(int tableSize) {
+    public int[][] calculateMultiplication(int tableSize) {
 
-        String[][] tableValues = new String[tableSize + 1][tableSize + 1];
+        int[][] tableValues = new int[tableSize + 1][tableSize + 1];
 
         // Формируем вертикальную ось таблицы
         for (int i = 0; i < tableValues.length; i++) {
-            for (int j = 0; j < 1; j++) {
-                tableValues[i][j] = String.valueOf(i);
-            }
+            tableValues[i][0] = i;
         }
 
         // Формируем горизонтальную ось таблицы
-        for (int i = 0; i < 1; i++) {
-            for (int j = 0; j < tableValues[i].length; j++) {
-                tableValues[i][j] = String.valueOf(j);
-            }
+        for (int j = 0; j < tableValues[0].length; j++) {
+            tableValues[0][j] = j;
         }
 
         // Заполняем таблицу значениями
         for (int i = 1; i < tableValues.length; i++) {
             for (int j = 1; j < tableValues[i].length; j++) {
-                tableValues[i][j] = String.valueOf(i * j);
+                tableValues[i][j] = i * j;
             }
         }
 
