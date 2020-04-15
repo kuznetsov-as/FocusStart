037eba4be4757131ecd5e7b5fcb11155fcb26ab9
deusestmortuus <kuznetsov1398@gmail.com>
2020-04-15 19:22:31 +0700

    Дополнил вывод в README.md, исправил имя теста в TaskTest

--- task4/src/test/java/cft/focus/task/TaskTest.java
+++ task4/src/test/java/cft/focus/task/TaskTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
 public class TaskTest {
 
     @Test
-    public void getResult_returnsResult_IfPass1and10_000_000() {
+    public void getResult_returnsCorrectResult_whenComputingOnLargeInterval() {
         Task task = new Task(1, 10_000_000);
         task.run();
         assertEquals(new  BigInteger("333333383333335000000"), task.getResult());
